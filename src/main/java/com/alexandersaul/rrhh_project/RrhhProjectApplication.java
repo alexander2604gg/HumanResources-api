98d6ce556769206730ddab384af6708b755cbe54
Alexander Saul <alexandersaul2604@gmail.com>
2024-10-12 17:46:34 -0500

    first commit

---
+++ src/main/java/com/alexandersaul/rrhh_project/RrhhProjectApplication.java
@@ -0,0 +1,13 @@
+package com.alexandersaul.rrhh_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RrhhProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RrhhProjectApplication.class, args);
+	}
+
+}
