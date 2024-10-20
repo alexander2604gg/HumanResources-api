4643c5ccb6262db0a7cd8f0a23d6a8a4f5774580
Alexander Saul <alexandersaul2604@gmail.com>
2024-10-19 22:42:21 -0500

    add filter chain

--- src/main/java/com/alexandersaul/rrhh_project/controller/EmployeeController.java
+++ src/main/java/com/alexandersaul/rrhh_project/controller/EmployeeController.java
@@ -1,5 +1,8 @@
 package com.alexandersaul.rrhh_project.controller;
 
+import com.alexandersaul.rrhh_project.dto.employee.EmployeeRegisterDto;
+import com.alexandersaul.rrhh_project.service.IEmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -7,14 +10,19 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/employees")
 public class EmployeeController {
 
+    @Autowired
+    private IEmployeeService employeeService;
+
     @GetMapping
     public ResponseEntity<Void> getAllEmployees() {
         return ResponseEntity.ok().build();
     }
 
     @PostMapping
-    public ResponseEntity<Void> registerEmployee() {
-        return ResponseEntity.ok().build();
+    public ResponseEntity<String> registerEmployee(@RequestBody EmployeeRegisterDto employeeRegisterDto) {
+        employeeService.registerEmployee(employeeRegisterDto);
+        return ResponseEntity.ok()
+                .body("Employee created");
     }
 
     @PutMapping("/{id}")
---
+++ src/main/java/com/alexandersaul/rrhh_project/security/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.alexandersaul.rrhh_project.security.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain (HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
+    }
+
+
+}
