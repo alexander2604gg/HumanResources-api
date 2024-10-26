df38f85b014cab3942a244d50dec4bdc96dbe2b7
Alexander Saul <alexandersaul2604@gmail.com>
2024-10-26 12:33:39 -0500

    implementacion del controller de permissions

---
+++ src/main/java/com/alexandersaul/rrhh_project/constants/PermissionConstants.java
@@ -0,0 +1,8 @@
+package com.alexandersaul.rrhh_project.constants;
+
+public class PermissionConstants {
+    public static final String  STATUS_201 = "201";
+    public static final String  MESSAGE_201 = "Permiso registrado con éxito";
+    public static final String  STATUS_200 = "200";
+    public static final String  MESSAGE_200 = "Permiso actualizado con éxito";
+}
